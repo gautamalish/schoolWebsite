c4d27490bb6589288efe97459c26c8518226c7f8
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-09-11 12:20:44 +0545

    a bit of responsiveness added

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -19,17 +19,17 @@ const Home = () => {
             <div>
               <p className="menu">MENU</p>
             </div>
-            <div className="absolute top-5 left-1/2 w-fit transform -translate-x-1/2 max-sm:hidden">
+            <div className="absolute top-5 left-1/2 w-fit transform -translate-x-1/2 max-md:hidden">
               <img src={bkvmLogo} alt="Logo" className=" top-0 m-auto w-2/3" />
             </div>
             <p className="admission">ADMISSION</p>
           </div>
           <div className="w-full items-center gap-10">
             <div className="absolute top-[30%] left-1/2 -translate-x-1/2 flex flex-col w-full justify-center items-center gap-10">
-              <p className="text-white text-2xl font-normal tracking-[0.2rem]">
+              <p className="text-white text-2xl text-center font-normal tracking-[0.2rem]">
                 BAL KALYAN VIDYA MANDIR
               </p>
-              <p className="text-white text-7xl tracking-[0.2rem] font-bold font-comfortaa">
+              <p className="text-white text-4xl md:text-7xl tracking-[0.2rem] font-bold font-comfortaa text-center">
                 A Clear Path for Potential
               </p>
               <button className="bg-red-700 text-white py-5 px-7 tracking-[0.2rem] font-sans font-normal text-xl">
@@ -37,7 +37,7 @@ const Home = () => {
               </button>
               <div className="w-[80%] bg-gray-400 h-[1px] mt-40"></div>
             </div>
-            <div className="flex gap-10 w-[80%] absolute left-1/2 -translate-x-1/2 bottom-5">
+            <div className="flex gap-10 w-[80%] absolute left-1/2 -translate-x-1/2 bottom-5 max-md:hidden">
               <div className="flex flex-col justify-center items-center flex-1 gap-3">
                 <p className="text-white font-comfortaa tracking-[0.2rem] text-xl">
                   MIND
